c446c62ba3b16c69afb076c831e0bb4c499cdb84
aditya gupta <aditya.gupta@digital.datamatics.com>
2021-12-22 21:25:15 +0530

    excel file write operation done

--- src/main/java/com/intelliatech/helper/ExcelOperation.java
+++ src/main/java/com/intelliatech/helper/ExcelOperation.java
@@ -9,13 +9,13 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 @Singleton
 public class ExcelOperation {
+
 
     private static int failed = 0;
     private static int pass = 0;
@@ -88,13 +88,16 @@ public class ExcelOperation {
     }
     public static void main(String[] args) {
 
-        String tempName = "khan";
-        excelOperation(tempName);
-        System.out.println("failed = " + failed);
-        System.out.println("pass   = " + pass);
-        System.out.println("total  = "+ total );
-        System.out.println(tempName + " = "+lastName);
+//        String tempName = "khan";
+//        excelOperation(tempName);
+//        System.out.println("failed = " + failed);
+//        System.out.println("pass   = " + pass);
+//        System.out.println("total  = "+ total );
+//        System.out.println(tempName + " = "+lastName);
+//        getExcelFile();
+        getDatabaseExcel();
     }
+
 
 
     //check that file is of excel type or not
@@ -166,4 +169,101 @@ public class ExcelOperation {
         return list;
     }
 
+
+    public static void getExcelFile()
+    {
+
+        // Blank workbook
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        // Create a blank sheet
+        XSSFSheet sheet = workbook.createSheet("user Details");
+
+        // This data needs to be written (Object[])
+        Map<String, Object[]> data = new TreeMap<>();
+        data.put("1", new Object[]{ "ID", "NAME", "LASTNAME" });
+        data.put("2", new Object[]{ 1, "Pankaj", "Kumar" });
+        data.put("3", new Object[]{ 2, "Prakashni", "Yadav" });
+        data.put("4", new Object[]{ 3, "Ayan", "Mondal" });
+        data.put("5", new Object[]{ 4, "Virat", "kohli" });
+
+        // Iterate over data and write to sheet
+        Set<String> keyset = data.keySet();
+        int rownum = 0;
+        for (String key : keyset) {
+            // this creates a new row in the sheet
+            Row row = sheet.createRow(rownum++);
+            Object[] objArr = data.get(key);
+            int cellnum = 0;
+            for (Object obj : objArr) {
+                // this line creates a cell in the next column of that row
+                Cell cell = row.createCell(cellnum++);
+                if (obj instanceof String)
+                    cell.setCellValue((String)obj);
+                else if (obj instanceof Integer)
+                    cell.setCellValue((Integer)obj);
+            }
+        }
+        try {
+
+            FileOutputStream out = new FileOutputStream(new File("E:\\excel\\abc.xlsx"));
+            workbook.write(out);
+            out.close();
+            System.out.println("gfgcontribute.xlsx written successfully on disk.");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    public static void getDatabaseExcel()
+    {
+
+        try {
+
+
+            List<User> list = new ArrayList<>();
+            list.add(new User(1, "adnan", null, 20));
+            list.add(new User(2, "dheeraj", "8964882357", 25));
+            list.add(new User(3, "manoj", "8964882355", 26));
+
+            String[] columns = {"mobile_number", "user_age", "user_name"};
+
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            XSSFSheet sheet = workbook.createSheet("User Data");
+            Row headerRow = sheet.createRow(0);
+
+            for (int i = 0; i < columns.length; ++i) {
+                Cell cell = headerRow.createCell(i);
+                cell.setCellValue(columns[i]);
+            }
+
+            int rowNumber = 1;
+
+            for (User u : list) {
+                Row row = sheet.createRow(rowNumber++);
+                row.createCell(0).setCellValue(u.getMobileNumber());
+                row.createCell(1).setCellValue(u.getUserAge());
+                row.createCell(2).setCellValue(u.getUserName());
 }
+
+            for (int i = 0; i < columns.length; i++) {
+                sheet.autoSizeColumn(i);
+            }
+
+        FileOutputStream fileOut = new FileOutputStream("E:\\excel\\mno.csv");
+        workbook.write(fileOut);
+        fileOut.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+}}
